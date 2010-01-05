bd9c36335e58de0eb5814b9bbc3e011cdfb2abbd
Diego Salazar <mastermindxs@gmail.com>
2010-01-05 15:12:12 -0500

    removed require PP line from app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-require 'PP' # allows the user of pretty_print
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
