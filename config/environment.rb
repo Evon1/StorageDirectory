e6bd99db0ff58ad2a86cdb5f5c088e07afc2b465
Diego Salazar <diego@greyrobot.com>
2010-04-06 13:33:11 -0400

    removed authlogic as a gem

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   #config.gem 'liquid'
-  config.gem 'authlogic'
+  #config.gem 'authlogic'
   #config.gem 'facebooker'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
