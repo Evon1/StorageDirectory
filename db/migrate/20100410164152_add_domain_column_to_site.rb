60d06738319d3939ddc1ae690455aecd41e201c1
Diego Salazar <diego@greyrobot.com>
2010-04-10 21:34:35 -0400

    removed some files

--- db/migrate/20100410164152_add_domain_column_to_site.rb
+++
@@ -1,9 +0,0 @@
-class AddDomainColumnToSite < ActiveRecord::Migration
-  def self.up
-    add_column :sites, :domain, :string
-  end
-
-  def self.down
-    remove_column :sites, :domain
-  end
-end
