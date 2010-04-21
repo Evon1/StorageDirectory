965a3ae3524ecc86d66abe3c660bf69ed2cca266
Diego Salazar <diego@greyrobot.com>
2010-04-21 18:49:53 -0400

    listing assoc partials

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,9 @@
 module ListingsHelper
+  
+  def greyresult_panel_template(listing, &content)
+    html  = "\n<h5 class=\"white dark_text_shadow\">#{listing.title}</h5>\n<div class=\"inner border_box\">"
+      html << yield
+    html << "</div>\n"
+  end
+  
 end
