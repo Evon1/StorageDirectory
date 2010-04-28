3e975afc042bd3ec8c15f78888b6856c2fa088f4
Diego Salazar <diego@greyrobot.com>
2010-04-28 09:21:18 -0400

    migration fix

--- db/migrate/20100428074941_lat_lng_precision.rb
+++ db/migrate/20100428074941_lat_lng_precision.rb
@@ -3,13 +3,6 @@ class LatLngPrecision < ActiveRecord::Migration
     remove_column :maps, :lat
     remove_column :maps, :lng
     
-    begin
-      remove_column :maps, :nlat
-      remove_column :maps, :nlng
-    rescue
-      nil
-    end
-    
     add_column :maps, :lat, :float, :precision => 8
     add_column :maps, :lng, :float, :precision => 8
   end
