2ebd1b442a5e9e8e30660dde5d469527d65d8486
Diego Salazar <diego@greyrobot.com>
2010-04-28 01:11:37 -0400

    removed distance column and migr

--- db/migrate/20100423023544_add_distance_column_to_map.rb
+++
@@ -1,9 +0,0 @@
-class AddDistanceColumnToMap < ActiveRecord::Migration
-  def self.up
-    add_column :maps, :distance, :integer
-  end
-
-  def self.down
-    remove_column :maps, :distance
-  end
-end
