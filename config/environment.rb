d983c2b417b62e720e210d78d1d09d57ced4ebf5
Diego Salazar <diego@Diego-iMac.local>
2010-07-06 17:47:23 -0400

    fixed some gem issues

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present=
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -24,7 +24,7 @@ Rails::Initializer.run do |config|
   # config.gem 'facebooker'
   config.gem 'geokit'
   config.gem 'will_paginate'
-  config.gem 'ajaxful_rating'
+  config.gem 'ajaxful_rating', :version => '2.2.8'
   config.gem 'memcached'
   require 'memcached'
   
