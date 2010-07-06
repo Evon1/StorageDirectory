33957946dd44fc016cacfeaa1f5a8dc05d6afb96
Diego Salazar <diego@Diego-iMac.local>
2010-07-06 17:40:46 -0400

    cal icon

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   # config.gem 'liquid'
-  # config.gem 'authlogic'
+   config.gem 'authlogic'
   # config.gem 'facebooker'
   config.gem 'geokit'
   config.gem 'will_paginate'
