71e4a301cd0fb3e9455f43622479a7530465417c
Diego Salazar <diego@Diego-iMac.local>
2010-07-06 17:37:26 -0400

    cal icon

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,7 @@ Rails::Initializer.run do |config|
   config.gem 'will_paginate'
   config.gem 'ajaxful_rating'
   config.gem 'memcached'
-  #require 'memcached'
+  require 'memcached'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
