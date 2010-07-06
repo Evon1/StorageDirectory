13d0db693adb8d42628da30f66bdd9b02d11ea02
Diego Salazar <diego@Diego-iMac.local>
2010-07-06 11:39:22 -0400

    memcached gem, updated app_config

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,8 @@ Rails::Initializer.run do |config|
   config.gem 'geokit'
   config.gem 'will_paginate'
   config.gem 'ajaxful_rating'
+  config.gem 'memcached'
+  require 'memcached'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
