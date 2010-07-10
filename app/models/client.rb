1ff31f249f945152d83fe4286fd89dfb954d0564
Diego Salazar <diego@Diego-iMac.local>
2010-07-09 20:00:06 -0400

    some client edit work

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < User
   
-  has_many :listings
+  has_many :listings, :foreign_key => 'user_id'
   accepts_nested_attributes_for :listings
   
   attr_accessor :first_name, :last_name
